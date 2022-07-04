476754bce6bd9df10b8753f2bc1183a18e9c3fb6
Ivan <ivanbdn@yandex.ru>
2022-07-04 04:44:18 +0300

    preparations for test

---
+++ src/components/FloorItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="post">
+        <div>Пространство шахты</div>
+        <shaft-space v-for="index in elevatorCount"/>
+        Этаж
+        <elevator-button>Вызов лифта</elevator-button>
+    </div>
+</template>
+
+<script>
+// import MyButton from '@/components/UI/MyButton.vue';
+import ElevatorButton from '@/components/UI/ElevatorButton.vue';
+import ShaftSpace from '@/components/ShaftSpace.vue';
+
+export default {
+    components: {ElevatorButton, ShaftSpace},
+    props: {
+        post: {
+            type: Object,
+            required: true
+        },
+        elevatorCount: {
+            type: Number,
+            required: true
+        },
+    }
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+</style>
---
+++ src/components/ShaftSpace.vue
@@ -0,0 +1,29 @@
+<template>
+        <div>Пространство шахты</div>
+</template>
+
+<script>
+
+export default {
+    components: {},
+    props: {
+        // post: {
+        //     type: Object,
+        //     required: true
+        // }
+        
+    }
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+</style>
